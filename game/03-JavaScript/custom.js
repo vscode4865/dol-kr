4ee173536ff75f35ea93d4016c3996235bdd1a18
uotalkie <uotalkie@naver.com>
2021-08-28 22:19:20 +0900

    번역버전 0.1.1.0828
    - nnpc_gendery_ 버그 수정 (긴급) ( https://arca.live/b/textgame/32877267 )

--- game/03-JavaScript/custom.js
+++ game/03-JavaScript/custom.js
@@ -1,6 +1,6 @@
 /*window*/
 window.Customconfig = {
-    "version" : "0.1.1.0827"
+    "version" : "0.1.1.0828"
 }
 
 l10nStrings.errorTitle = StartConfig.version + "-" + Customconfig.version + " Error";
