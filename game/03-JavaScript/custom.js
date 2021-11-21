9ebd49a1281d771ee1f0663d63d1aae140f2f708
uotalkie <uotalkie@naver.com>
2021-11-22 00:14:53 +0900

    번역버전 0.1.1.1122
    - 번역버전 0.1.1.1122
    - 휘트니의 팔을 밀어낸다 버그 수정 ( https://arca.live/b/textgame/38748972 덧글 )
    - trWeather 버그 수정 ( https://arca.live/b/textgame/38755963 )
    - 잡담한다, 무언가 잘못되었는지 묻는다 버그 수정 ( https://arca.live/b/textgame/38755963 )
    - 간섭한다 버그 수정 ( https://arca.live/b/textgame/38755963 )
    - walletPost 버그 수정 ( https://arca.live/b/textgame/38771367 )

--- game/03-JavaScript/custom.js
+++ game/03-JavaScript/custom.js
@@ -1,6 +1,6 @@
 /*window*/
 window.Customconfig = {
-    "version" : "0.1.1.1121.2"
+    "version" : "0.1.1.1122"
 }
 
 l10nStrings.errorTitle = StartConfig.version + "-" + Customconfig.version + " Error";
