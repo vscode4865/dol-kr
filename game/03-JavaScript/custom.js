07d9d99c2bce8e2098c22fab8f0d33d6923e2e90
uotalkie <59263390+uotalkie@users.noreply.github.com>
2021-11-27 20:39:50 +0900

    번역버전 0.1.1.1127
    - 번역버전 0.1.1.1127
    - base-system/attitudes.twee 와 base-combat 번역 차이 수정
    - persons_i 버그 수정

--- game/03-JavaScript/custom.js
+++ game/03-JavaScript/custom.js
@@ -1,6 +1,6 @@
 /*window*/
 window.Customconfig = {
-    "version" : "0.1.1.1122"
+    "version" : "0.1.1.1127"
 }
 
 l10nStrings.errorTitle = StartConfig.version + "-" + Customconfig.version + " Error";
