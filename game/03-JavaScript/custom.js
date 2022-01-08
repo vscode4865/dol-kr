e11a274a3ace32a0da8efa2a287336203bf9c3e8
uotalkie <59263390+uotalkie@users.noreply.github.com>
2022-01-08 22:48:01 +0900

    번역버전 0.1.2022.0108
    - 번역버전 0.1.2022.0108
    - BodypartSelectionErr 버그 수정 ( https://arca.live/b/textgame/42007371 )
    - 영문자 남아있던 것 수정 ( https://arca.live/b/textgame/42007371 )

--- game/03-JavaScript/custom.js
+++ game/03-JavaScript/custom.js
@@ -1,6 +1,6 @@
 /*window*/
 window.Customconfig = {
-    "version" : "0.1.1.1231"
+    "version" : "0.1.2022.0108"
 }
 
 l10nStrings.errorTitle = StartConfig.version + "-" + Customconfig.version + " Error";
