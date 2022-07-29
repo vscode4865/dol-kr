15eeafa337178d0c1c224e55fadadaefa58a6bf5
uotalkie <59263390+uotalkie@users.noreply.github.com>
2022-07-29 20:44:46 +0900

    번역버전 0.1.2022.0729.2
    - apk 파일 제작 에러나는 문제 수정 ( https://arca.live/b/textgame/55396024 )
    - wordify_ 버그 수정 ( https://arca.live/b/textgame/55401079 )

--- game/03-JavaScript/custom.js
+++ game/03-JavaScript/custom.js
@@ -1,6 +1,6 @@
 /*window*/
 window.Customconfig = {
-    "version" : "0.1.2022.0729"
+    "version" : "0.1.2022.0729.2"
 }
 
 l10nStrings.errorTitle = StartConfig.version + "-" + Customconfig.version + " Error";
