8ba48345adc895f11d93fc995b4711cc94187af6
uotalkie <59263390+uotalkie@users.noreply.github.com>
2023-01-28 22:43:01 +0900

    번역버전 0.1.2023.0128
    - 번역버전 0.1.2023.0128
    - EasyPost 구버전 삭제준비

--- game/03-JavaScript/custom.js
+++ game/03-JavaScript/custom.js
@@ -1,6 +1,6 @@
 /*window*/
 window.Customconfig = {
-    "version" : "0.1.2023.0115"
+    "version" : "0.1.2023.0128"
 }
 
 l10nStrings.errorTitle = StartConfig.version + "-" + Customconfig.version + " Error";
