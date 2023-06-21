090d0e1d4caa1c2f76984f6f7bffd002619bab46
uotalkie <59263390+uotalkie@users.noreply.github.com>
2023-06-21 23:04:32 +0900

    원본 0.4.0.9 적용, 번역버전 0.1.2023.0621
    - 원본 0.4.0.9 적용 ( https://gitgud.io/Vrelnir/degrees-of-lewdity/-/tree/1652c1af6b809cd32d3f8adcf4756106196d8217 )
    - 업데이트에 따른 번역 추가/수정
    - 번역버전 0.1.2023.0621

--- game/03-JavaScript/custom.js
+++ game/03-JavaScript/custom.js
@@ -1,6 +1,6 @@
 /*window*/
 window.Customconfig = {
-    "version" : "0.1.2023.0616"
+    "version" : "0.1.2023.0621"
 }
 
 l10nStrings.errorTitle = StartConfig.version + "-" + Customconfig.version + " Error";
