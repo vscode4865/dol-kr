66335d5238fad466cccd3514b3845ee8b76d519e
uotalkie <59263390+uotalkie@users.noreply.github.com>
2023-07-22 21:57:09 +0900

    번역버전 0.1.2023.0722
    - 번역버전 0.1.2023.0722

--- game/03-JavaScript/custom.js
+++ game/03-JavaScript/custom.js
@@ -1,6 +1,6 @@
 /*window*/
 window.Customconfig = {
-    "version" : "0.1.2023.0714"
+    "version" : "0.1.2023.0722"
 }
 
 l10nStrings.errorTitle = StartConfig.version + "-" + Customconfig.version + " Error";
