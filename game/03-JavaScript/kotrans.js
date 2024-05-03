58822918c9e1f7c182c4d9fd3bf6ad9b005ee989
uotalkie <59263390+uotalkie@users.noreply.github.com>
2024-05-03 13:46:12 +0900

    번역버전 0.1.2024.0503
    - 긴급 오류 수정
    - 번역버전 0.1.2024.0503

--- game/03-JavaScript/kotrans.js
+++ game/03-JavaScript/kotrans.js
@@ -1,6 +1,6 @@
 /* Korean translation information */
 window.koTrans = {
-	"version": "0.1.2024.0502.1",
+	"version": "0.1.2024.0503",
 	
 	"info": 
 		'한국어 번역판<span class="blue"><<link "(?)">><<run koTrans.legal()>><</link>></span><br>'+
