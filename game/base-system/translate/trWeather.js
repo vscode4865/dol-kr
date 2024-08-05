e3a1be6ac23bba6ef3e17c96d79a6dd3ec11f3d5
vscode4865 <vscode89@gmail.com>
2024-08-06 02:28:23 +0900

    원본 맞춰서 업데이트

--- game/base-system/translate/trWeather.js
+++ game/base-system/translate/trWeather.js
@@ -12,12 +12,18 @@ function trWeather(weather, post, sep)
 		"heavyPrecipitation":{ko:"눈비 많음",post:0},
 		"thunderStorm":{ko:"번개폭풍",post:0},
 	};
-    T.trResult = weatherList[weather].ko;
+    let found = weatherList[weather];
+	if (found)
+	{
+		T.trResult = found.ko;
 
-    if (post)
-        trPost(weatherList[weather].post, post, sep);
+		if (post)
+			trPost(found.post, post, sep);
+	}
+	else
+		T.trResult = `<span class='red'>에러: trWeather: weatherList에 정의되어 있지 않음: ${weather}</span>`;
 
-	return T.trResult;
+		return (found?T.trResult:undefined);
 }
 window.trWeather = trWeather;
 DefineMacro("trWeather", trWeather);
